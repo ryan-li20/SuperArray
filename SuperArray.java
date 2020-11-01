6b5faf4a0fe517babc79268c05dac05d3b690d37
ryan-li20 <rli20@stuy.edu>
2020-11-01 12:30:39 -0500

    before the line

---
+++ SuperArray.java
@@ -0,0 +1,29 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+
+  public SuperArray(){
+    data = new String[10];
+    size = 0;
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String element){
+    data[size] = element;
+    size ++;
+    return true;
+  }
+
+  public String get(int index){
+    return data[index];
+  }
+
+  public String set(int index, String element){
+    String temp = data[index];
+    data[index] = element;
+    return temp;
+  }
+}
