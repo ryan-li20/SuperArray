532314811712bc1cb7fc5381ab442a192eca8a38
ryan-li20 <rli20@stuy.edu>
2020-11-03 13:46:41 -0500

    classwork donow

--- SuperArray.java
+++ SuperArray.java
@@ -37,4 +37,36 @@ public class SuperArray{
     }
     data = newguy;
   }
+
+  public boolean isEmpty(){
+    return size == 0;
+  }
+
+  public void clear(){
+    size = 0;
+    data = new String[10];
+  }
+
+  public boolean contains(String s){
+    if (isEmpty()){
+      return false;
+    }
+    for(int i = 0; i < size; i++){
+      if(data[i].equals(s)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String toString(){
+    if(isEmpty()){
+      return "[]";
+    }
+    String output1 = "[" + data[0];
+    for(int i = 1; i < size; i++){
+      output1 = output1 + ", " + data[i];
+    }
+    return output1 + "]";
+  }
 }
